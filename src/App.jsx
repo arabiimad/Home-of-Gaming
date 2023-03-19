a3de3973a08faad4e90ce15088a740a013066ff0
MeReda <medredaben14@gmail.com>
2023-03-19 11:51:51 +0000

    prepare envirenment

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <>
+      <div className="row h-100 align-items-center">
+        <div className="col text-center border border-primary rounded-end-5 p-5 m-5">
+          <span className="text-primary text-center fs-1 fw-bold">primary</span>
+          <span className="d-block mx-auto square bg-primary rounded-pill m-3"></span>
+          <button className="btn btn-primary m-1">button</button>
+          <button className="btn btn-outline-primary m-1">button</button>
+        </div>
+        <div className="col text-center border border-secondary rounded-start-5 p-5 m-5">
+          <span className="text-secondary text-center fs-1 fw-bold">
+            secondary
+          </span>
+          <span className="d-block mx-auto square bg-secondary rounded-pill m-3"></span>
+          <button className="btn btn-secondary m-1">button</button>
+          <button className="btn btn-outline-secondary m-1">button</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   );
 }
 
